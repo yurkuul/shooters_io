b2a45a50c5e1e0a2c9a2c91d5c080588240eb91d
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2022-12-22 10:52:39 -0600

    Create Birds.java

---
+++ Birds.java
@@ -0,0 +1,38 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Birds fly across the screen and drop tokens with
+ * different effects.
+ * 
+ * Lisa Zhu
+ * 2020/01
+ */
+public class Birds extends MyActor
+{
+    private long timeDelay;  //Time between each frame rate in animation
+    private int frame = 1;
+    private int animationCount = 0;
+    
+    public Birds() {
+        setImage ("Bird 1.png");
+    }
+    
+    public void act() {
+
+    }    
+    
+    private void movement() {
+
+    }
+    private void switchImages() {
+        
+    }
+    
+    private void dropTokens() {
+        
+    }
+    
+    private void checkAtEdge() {
+        
+    }
+}
