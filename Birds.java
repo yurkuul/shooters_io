ef8ce481b017b9734af657b0c4506680a6a5b313
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2022-12-23 18:11:19 -0600

    Update Birds.java

--- Birds.java
+++ Birds.java
@@ -17,15 +17,38 @@ public class Birds extends MyActor
         setImage ("Bird 1.png");
     }
     
-    public void act() {
-
+    public void act() 
+    {
+        movement();
+        animationCount++;
     }    
     
     private void movement() {
-
+        move (4);
+        
+        if (animationCount % 6 == 0) {  //divides animation counter by 6 only if there will be a remainder of 0
+            switchImages();
+        }
     }
     private void switchImages() {
-        
+        //image variables
+        GreenfootImage Bird1 = new GreenfootImage ("Bird 1.png");
+        GreenfootImage Bird2 = new GreenfootImage ("Bird 2.png");
+        GreenfootImage Bird3 = new GreenfootImage ("Bird 3.png");
+        GreenfootImage Bird4 = new GreenfootImage ("Bird 4.png");
+        //Animating the bird
+        if (frame == 1) {
+            setImage (Bird1);
+        } else if (frame == 2) {
+            setImage (Bird2);
+        } else if (frame == 3) {
+            setImage (Bird3);
+        } else if (frame == 4) {
+            setImage (Bird4);
+            frame = 1;
+            return;  //stops the code from executing again
+        }
+        frame ++;  //Increases the frame
     }
     
     private void dropTokens() {
